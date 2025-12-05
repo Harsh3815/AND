64157459728aa943f9da8f66cf4e07d4c2c9a793
Harsh3815 <harshkatrodiya336@gmail.com>
2025-12-05 11:26:13 +0530

    Add files via upload

---
+++ biodata.kt
@@ -0,0 +1,21 @@
+
+//program to display biodata of person and print it.
+fun main() {
+
+    var no = 10
+    println("Value is :$no")
+    print("enter name:")
+    var name = readln()
+
+    print("enter age:")
+    var age = readln()
+
+    print("enter city:")
+    var city = readln()
+
+
+    println("Name: $name")
+    println("Age: $age")
+    println("City: $city")
+
+}
